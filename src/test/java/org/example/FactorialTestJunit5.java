c1195cb97ad09ffa18adcd61614f95d6b407e10e
ArturMukhamadeev <praddartur@gmail.com>
2025-02-10 22:36:11 +0300

    Исправлен код Factorial

--- src/test/java/org/example/FactorialTestJunit5.java
+++ src/test/java/org/example/FactorialTestJunit5.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FactorialTestJunit5 {
+    @Test
+    void testFactorialZero() {
+        assertEquals(1, Factorial.compute(0));
+    }
+
+    @Test
+    void testFactorialOne() {
+        assertEquals(1, Factorial.compute(1));
+    }
+
+    @Test
+    void testFactorialPositive() {
+        assertEquals(120, Factorial.compute(5));
+    }
+
+    @Test
+    void testFactorialNegative() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> Factorial.compute(-1));
+        assertEquals("Факториал отрицательного числа не определен.", exception.getMessage());
+    }
 }
