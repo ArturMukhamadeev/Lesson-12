3b323102ad36b47a1518a679f2f00dd244425c8f
ArturMukhamadeev <praddartur@gmail.com>
2025-02-10 22:36:11 +0300

    Исправлен код Factorial

--- src/test/java/org/example/FactorialTestTestNG.java
+++ src/test/java/org/example/FactorialTestTestNG.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
+
 public class FactorialTestTestNG {
+    @Test
+    public void testFactorialZero() {
+        assertEquals(Factorial.compute(0), 1);
+    }
+
+    @Test
+    public void testFactorialOne() {
+        assertEquals(Factorial.compute(1), 1);
+    }
+
+    @Test
+    public void testFactorialPositive() {
+        assertEquals(Factorial.compute(5), 120);
+    }
+
+    @Test
+    public void testFactorialNegative() {
+        assertThrows(IllegalArgumentException.class, () -> Factorial.compute(-1));
+    }
 }
